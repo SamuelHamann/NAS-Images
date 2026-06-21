5a44b0f87a76451ed6571e0479b2353c8c6ac189
Samuel Hamann <shamann4242@gmail.com>
2026-06-20 20:47:23 -0400

    Fix postgres

---
+++ postgres/commands_to_run.sh
@@ -0,0 +1,13 @@
+# 1. Stop the container
+docker compose -f /volume1/docker/postgres/compose.yaml down
+
+# 2. Wipe any partial/broken pgdata from previous failed starts
+sudo rm -rf /volume1/docker/postgres/data/pgdata
+
+# 3. Give UID 1000 ownership of the data directory
+sudo chown -R 1000:1000 /volume1/docker/postgres/data
+sudo chmod 700 /volume1/docker/postgres/data
+
+# 4. Start fresh
+docker compose -f /volume1/docker/postgres/compose.yaml up -d
+docker compose -f /volume1/docker/postgres/compose.yaml logs -f
